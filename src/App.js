4474ddfb4242ae3a44a279a02ba6d4f147a7c83b
Maicol <pessoal.maicol@gmail.com>
2023-09-23 19:06:40 -0300

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [cep, setCep] = useState('');
+  const [data, setData] = useState('');
+
+  const handleInput = (e) => {
+    setCep(e.target.value);
+  };
+
+  const handleSearch = async () => {
+    try {
+      const response = await api.get(`${cep}/json`);
+      setData(response.data);
+      setCep(''); // Limpa campo do input
+
+    } catch (error) {
+      alert("Ops, erro ao buscar cep");
+      setCep(''); // Limpa campo do input
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input
+          onChange={handleInput}
+          type="text"
+          placeholder="Digite seu cep"
+          value={cep}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="black" />
+        </button>
+      </div>
+      {data ? ( // Se tiver dados renderiza os dados de cep
+        <div className="main">
+          <h2>CEP: {data.cep}</h2>
+          <p>Rua: {data.logradouro}</p>
+          <p>Complemento: {data.complemento}</p>
+          <p>Bairro: {data.bairro}</p>
+          <p>Cidade: {data.localidade}</p>
+          <p>Estado: {data.uf}</p>
+        </div>
+      ) : null}
     </div>
   );
 }
